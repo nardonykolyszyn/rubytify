bee5997681842581af3b8fca7e3ee493b8addbe8
devpolish <nardonykolyszyn@gmail.com>
2019-11-14 13:41:06 -0500

    enable routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :artists, only: :index do
+        get :albums, on: :member
+      end
+      
+      # Member actions cannot be used outside resource scope.
+      resources :albums, except: %i[index show new edit create destroy update] do
+        get :songs, on: :member
+      end
+
+      resources :genres, except: %i[index show new edit create destroy update] do
+        get :random_song
+      end
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,8 @@
+# frozen_string_literal: true
 
+
+# It loads artists seed file by combining IO and YAML.
+# Deep symbolize is extended from ActiveSupport, it helps to
+# implicitily convert string keys to symbols for performance purposes
+ARTISTS = YAML.load_file('config/seed.yml')
+              .deep_symbolize_keys.freeze
