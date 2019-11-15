a22d343475236e19fbd7d37b8b2ef541fe82c330
devpolish <nardonykolyszyn@gmail.com>
2019-11-15 06:59:06 -0500

    remove sprockets

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,6 @@ require "rails"
 require "active_model/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
-require "sprockets/railtie"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
