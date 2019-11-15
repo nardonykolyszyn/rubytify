6f6501d318e365e1a088ebfae66d7edaad237756
devpolish <nardonykolyszyn@gmail.com>
2019-11-15 07:00:25 -0500

    load artists from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,10 @@
 # It loads artists seed file by combining IO and YAML.
 # Deep symbolize is extended from ActiveSupport, it helps to
 # implicitily convert string keys to symbols for performance purposes
-ARTISTS = YAML.load_file('config/seed.yml')
-              .deep_symbolize_keys.freeze
+ARTISTS = YAML.load_file('config/seed.yml').deep_symbolize_keys[:artists].map(&:to_s).freeze
+
+ARTISTS.each do |artist_name|
+  operation_result = RegisterArtist.call(
+    artist: artist_name
+  )
+end
