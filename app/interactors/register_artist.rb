874f9c52c8f4f6ec0250d2718aa4681be5b04df5
devpolish <nardonykolyszyn@gmail.com>
2019-11-15 07:00:44 -0500

    set attributes

--- app/interactors/register_artist.rb
+++ app/interactors/register_artist.rb
@@ -2,5 +2,32 @@
 
 class RegisterArtist < ApplicationInteractor
   def call
+    response
+    response.nil? ? not_found : persist_record(:artist, set_attributes)
+  end
+
+  private
+
+
+  # FIXME: Set attributes dinamically
+  def set_attributes
+    {
+      name: response.name,
+      image: response.images.first["url"],
+      genres: response.genres,
+      popularity: response.popularity,
+      spotify_url: response.href,
+      spotify_id: response.id
+    }
+  end
+
+  # NOTE: Lazy loading implementation.
+  def artist
+    @artist ||= context.artist
+  end
+  
+  # FIXME: Improve this lazy loading.
+  def response
+    @response ||= RSpotify::Artist.search(artist).first
   end
 end
