f57b0fd5e882c306ff875bda75bf9d52388d61a9
devpolish <nardonykolyszyn@gmail.com>
2019-11-15 20:59:05 -0500

    add messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'colorize'
 
 # It loads artists seed file by combining IO and YAML.
 # Deep symbolize is extended from ActiveSupport, it helps to
@@ -7,7 +8,11 @@
 ARTISTS = YAML.load_file('config/seed.yml').deep_symbolize_keys[:artists].map(&:to_s).freeze
 
 ARTISTS.each do |artist_name|
+  puts "Retrieving information for #{artist_name}...".green
   operation_result = RegisterArtist.call(
     artist_name: artist_name
   )
+
+  puts "Error: #{operation_result.error}".red if operation_result.failure?
+  puts 'Information has been retrieved'.green if operation_result.success?
 end
