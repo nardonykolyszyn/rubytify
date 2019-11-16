e70edcee2bc961fdfe44745a4c41a378ebca2cfb
devpolish <nardonykolyszyn@gmail.com>
2019-11-16 08:24:25 -0500

    assign resource

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -4,6 +4,8 @@ module Api
   module V1
     # It returns all song for an album.
     class AlbumsController < ApiSitesController
+      before_action :assign_resource, only: :songs
+
       def songs
         render json: {
           data: load_collection(@album.songs)
