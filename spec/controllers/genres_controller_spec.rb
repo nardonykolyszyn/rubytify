982ff96dde06ec0faf2dda3738c78df086026364
devpolish <nardonykolyszyn@gmail.com>
2019-11-16 12:50:15 -0500

    validate expected keys on success response

--- spec/controllers/genres_controller_spec.rb
+++ spec/controllers/genres_controller_spec.rb
@@ -3,10 +3,16 @@
 require 'rails_helper'
 
 describe 'return a random song', type: :request do
-  let(:default_params) { {format: :json} }
+  let(:default_params) { { format: :json } }
+  let(:expected_keys) { %w[name spotify_url preview_url explicit duration_ms] }
 
-  it 'return a empty array when unknown genre' do
+  it 'returns a empty array when unknown genre' do
     get '/api/v1/genres/93279834743/random_song'
     expect(JSON.parse(response.body)['data']).to eq([])
+  end
+
+  it 'returns an random song when genre is valid' do
+    get '/api/v1/genres/latin/random_song'
+    expect(JSON.parse(response.body)['data'].keys).to eq(expected_keys)
   end
 end
