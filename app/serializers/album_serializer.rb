a3bf61546125e1ae75ad7f325f324cf7ceb4c1cf
devpolish <nardonykolyszyn@gmail.com>
2019-11-15 21:00:02 -0500

    remove song attr

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 class AlbumSerializer < ActiveModel::Serializer
-  attributes %i[id name image total_tracks spotify_url spotify_id songs].freeze
-
-  def songs
-    ActiveModelSerializers::SerializableResource.new(object.songs)
-  end
+  attributes %i[id name image total_tracks spotify_url spotify_id].freeze
 end
