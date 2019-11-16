f2c5318f0050f5e540bb287f5c5e9b18f79f0d25
devpolish <nardonykolyszyn@gmail.com>
2019-11-16 08:27:24 -0500

    add top-level documentation

--- app/interactors/concerns/artist_extendable.rb
+++ app/interactors/concerns/artist_extendable.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# This module extends RSpotify parsing responses
+# It provides getter methods to descompose each response.
 module ArtistExtendable
   extend ActiveSupport::Concern
 
